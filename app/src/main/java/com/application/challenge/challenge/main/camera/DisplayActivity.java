bf8f266b2046d90e6f642f10c7a2963e4b15286c
Lucas Puterman <lucasputerman@gmail.com>
2015-03-13 11:42:54 -0300

    agregado botón de ir para atrás en un par de activities

--- app/src/main/java/com/application/challenge/challenge/main/camera/DisplayActivity.java
+++ app/src/main/java/com/application/challenge/challenge/main/camera/DisplayActivity.java
@@ -126,7 +126,7 @@ public class DisplayActivity extends ListActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_display, menu);
+        //getMenuInflater().inflate(R.menu.menu_display, menu);
         return true;
     }
 
@@ -135,12 +135,12 @@ public class DisplayActivity extends ListActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+//        int id = item.getItemId();
+//
+//        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
 
         return super.onOptionsItemSelected(item);
     }
--- app/src/main/java/com/application/challenge/challenge/main/picture/PictureActivity.java
+++ app/src/main/java/com/application/challenge/challenge/main/picture/PictureActivity.java
@@ -41,6 +41,9 @@ public class PictureActivity extends Activity {
 
         phObj = EventBus.getDefault().getStickyEvent(PhotoObject.class);
 
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+
+
         setContentView(R.layout.activity_picture);
 
     }
@@ -71,21 +74,19 @@ public class PictureActivity extends Activity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_picture, menu);
+        //getMenuInflater().inflate(R.menu.menu_picture, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+//        int id = item.getItemId();
+//
+//        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
 
         return super.onOptionsItemSelected(item);
     }
